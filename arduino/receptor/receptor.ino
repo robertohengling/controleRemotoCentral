f56eb2b7ec6899670f0d80e73d9258c48bf31d73
Roberto Palhares Hengling Pires <roberto.pires@matera.com>
2019-05-02 14:03:16 -0300

    Removendo comentários inúteis

--- arduino/receptor/receptor.ino
+++ arduino/receptor/receptor.ino
@@ -1,11 +1,3 @@
-/*
-* IRremote: IRrecvDump - dump details of IR codes with IRrecv
-* An IR detector/demodulator must be connected to the input RECV_PIN.
-* Version 0.1 July, 2009
-* Copyright 2009 Ken Shirriff
-* http://arcfn.com
-*/
-
 #include <IRremote.h>
 
 int RECV_PIN = 6;
@@ -17,14 +9,9 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn();
 }
 
-// Dumps out the decode_results structure.
-// Call this after IRrecv::decode()
-// void * to work around compiler issue
-//void dump(void *v) {
-// decode_results *results = (decode_results *)v
 void dump(decode_results *results) {
   int count = results->rawlen;
   if (results->decode_type == UNKNOWN) {
@@ -51,28 +38,28 @@ void dump(decode_results *results) {
    else if (results->decode_type == SAMSUNG) {
     Serial.print("Decoded SAMSUNG: ");
   }
-int val1 = results->value;
-Serial.print(val1, HEX);
-Serial.print(" (");
-int valbits = results->bits;
-Serial.print(valbits, DEC);
-Serial.println(" bits)");
-Serial.print("Raw (");
-Serial.print(count, DEC);
-Serial.print("): ");
+  int val1 = results->value;
+  Serial.print(val1, HEX);
+  Serial.print(" (");
+  int valbits = results->bits;
+  Serial.print(valbits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): ");
 
-for (int i = 0; i < count; i++) {
-  if ((i % 2) == 1) {
-    int valen = results->rawbuf[i]*USECPERTICK;
-    Serial.print(valen, DEC);
+  for (int i = 0; i < count; i++) {
+    if ((i % 2) == 1) {
+      int valen = results->rawbuf[i]*USECPERTICK;
+      Serial.print(valen, DEC);
+    }
+    else {
+      int negvalen =-(int)results->rawbuf[i]*USECPERTICK;
+      Serial.print(negvalen, DEC);
+    }
+    Serial.print(", ");
   }
-  else {
-    int negvalen =-(int)results->rawbuf[i]*USECPERTICK;
-    Serial.print(negvalen, DEC);
-  }
-  Serial.print(", ");
-}
-Serial.println("");
+  Serial.println("");
 }
 
 void loop() {
@@ -80,6 +67,6 @@ void loop() {
     int hexen = results.value;
     Serial.println(hexen, HEX);
     dump(&results);
-    irrecv.resume(); // Receive the next value
+    irrecv.resume();
   }
 }
